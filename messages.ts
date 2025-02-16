077bd679b23570da0204791075bce18cd817a5ef
HarmonySpace <jchavaspace@gmail.com>
2025-02-16 12:45:22 -0600

    feat(view): update messages
    - add new functions
    - update to work with theme.ts

--- messages.ts
+++ messages.ts
@@ -26,13 +26,20 @@ const showCancel = () => {
   console.log(colors.red("X  Operación cancelada"))
 }
 
+const showSuccess = () => {
+  console.clear()
+  console.log(colors.bgGreen.black("  Tema creado con éxito ⊂(´ W u W ⊂ )  "))
+  console.log("")
+  console.log("")
+}
+
 const showHelp = async () => {
   console.clear()
   console.log(colors.bgCyan.black("  Atajos ⊂(´ O _ O ⊂ )  "))
   console.log("")
   for(const [key, value] of keymaps.entries()) {
     const voidSpace = " ".repeat(maxKeySize - key.length);
-    console.log(key, voidSpace, " - ", value)
+    console.log(key, voidSpace, "   -   ", value)
   }
   for await (const event: KeyPressEvent of keypress()) {
     if (event.key === "?") {
@@ -41,28 +48,72 @@ const showHelp = async () => {
   }
 }
 
-const showTheme = async (c1, c2, c3, c4) => {
+const showTheme = async (c1, c2, c3, c4, theme) => {
   console.clear()
   console.log(colors.bgCyan.black("  Tema generado ⊂(´ ★ u ★ ⊂ )  "))
   console.log("")
-  const black = toHex(darken("#" + c1, 0.4)).slice(1)
-  const red = c1
-  const green = c2
-  const yellow = c3
-  const blue = c4
-  const magenta = toHex(mix("#" + c1, "#" + c4, 0.5)).slice(1)
-  const cyan = toHex(mix("#" + c2, "#" + c4, 0.5)).slice(1)
-  const white = toHex(lighten("#" + c1, 0.4)).slice(1)
+  const black = theme.black
+  const red = theme.red
+  const green = theme.green
+  const yellow = theme.yellow
+  const blue = theme.blue
+  const magenta = theme.magenta
+  const cyan = theme.cyan
+  const white = theme.white
+  const softBlack = theme.softBlack 
+  const softRed = theme.softRed
+  const softGreen= theme.softGreen
+  const softYellow = theme.softYellow
+  const softBlue = theme.softBlue
+  const softMagenta = theme.softMagenta
+  const softCyan = theme.softCyan
+  const softWhite = theme.softWhite
+  const background = theme.background 
+  const foreground = theme.foreground
+  const cursorColor = theme.cursorColor
+  const cursorText = theme.cursorText
+  const selectionBackground = theme.selectionBackground
+  const selectionForeground = theme.selectionForeground
   console.log(
-    colors.rgb24("██", parseInt(black, 16)), "0",
-    colors.rgb24("██", parseInt(red, 16)), "1",
-    colors.rgb24("██", parseInt(green, 16)), "2",
-    colors.rgb24("██", parseInt(yellow, 16)), "3",
-    colors.rgb24("██", parseInt(blue, 16)), "4",
-    colors.rgb24("██", parseInt(magenta, 16)), "5",
-    colors.rgb24("██", parseInt(cyan, 16)), "6",
-    colors.rgb24("██", parseInt(white, 16)), "7",
+    colors.rgb24("██", parseInt(black, 16)), " 0",
+    colors.rgb24("██", parseInt(red, 16)), " 1",
+    colors.rgb24("██", parseInt(green, 16)), " 2",
+    colors.rgb24("██", parseInt(yellow, 16)), " 3",
+    colors.rgb24("██", parseInt(blue, 16)), " 4",
+    colors.rgb24("██", parseInt(magenta, 16)), " 5",
+    colors.rgb24("██", parseInt(cyan, 16)), " 6",
+    colors.rgb24("██", parseInt(white, 16)), " 7",
   )
+  console.log(
+    colors.rgb24("██", parseInt(softBlack, 16)), " 8",
+    colors.rgb24("██", parseInt(softRed, 16)), " 9",
+    colors.rgb24("██", parseInt(softGreen, 16)), "10",
+    colors.rgb24("██", parseInt(softYellow, 16)), "11",
+    colors.rgb24("██", parseInt(softBlue, 16)), "12",
+    colors.rgb24("██", parseInt(softMagenta, 16)), "13",
+    colors.rgb24("██", parseInt(softCyan, 16)), "14",
+    colors.rgb24("██", parseInt(softWhite, 16)), "15",
+  )
+  console.log("")
+  console.log(
+    colors.rgb24("██", parseInt(background, 16)), "background",
+  )
+  console.log(
+    colors.rgb24("██", parseInt(foreground, 16)), "foreground",
+  )
+  console.log(
+    colors.rgb24("██", parseInt(cursorColor, 16)), "cursor-color",
+  )
+  console.log(
+    colors.rgb24("██", parseInt(cursorText, 16)), "cursor-text",
+  )
+  console.log(
+    colors.rgb24("██", parseInt(selectionBackground, 16)), "selection-background",
+  )
+  console.log(
+    colors.rgb24("██", parseInt(selectionForeground, 16)), "selection-foreground",
+  )
+
   for await (const event: KeyPressEvent of keypress()) {
     if (event.key === "t") {
       break 
@@ -70,4 +121,4 @@ const showTheme = async (c1, c2, c3, c4) => {
   }
 }
 
-export { showMenu, showColors, showCancel, showHelp, showTheme }
+export { showMenu, showColors, showCancel, showHelp, showTheme, showSuccess }
